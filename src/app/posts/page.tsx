c1076fdac850808035bf46bec24b0433b3c68299
Gabe <gabeshin.ts@gmail.com>
2023-10-12 20:55:54 +0200

    feat: add browser local cache for posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,6 +4,8 @@ import { IPost } from "@/interfaces/posts.interface";
 import PostItem from "@/components/posts/post-item";
 import { useEffect, useState } from "react";
 
+const CACHE_DURATION = 10 * 60 * 1000; // 10 minutes in milliseconds
+
 const PostPage = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
 
@@ -16,21 +18,34 @@ const PostPage = () => {
       }
 
       const data = await response.json();
-
       setPosts(data.data);
+
+      const cachedData = {
+        posts: data.data,
+        timestamp: new Date().getTime(),
+      };
+      localStorage.setItem("posts", JSON.stringify(cachedData));
     } catch (error) {
       console.error("Could not fetch posts:", error);
     }
   };
 
   useEffect(() => {
+    const cachedData = localStorage.getItem("posts");
+    if (cachedData) {
+      const { posts, timestamp } = JSON.parse(cachedData);
+      const now = new Date().getTime();
+      if (now - timestamp < CACHE_DURATION) {
+        setPosts(posts);
+        return;
+      }
+    }
     fetchPosts();
   }, []);
 
   return (
     <main>
       <SearchBar />
-      {/* create post-item list with dummyPosts */}
       <div className="flex flex-col rounded-sm ml-5 mr-5">
         {posts.map((post: IPost) => (
           <PostItem key={post.id} post={post} />
