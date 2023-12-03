2a75d49a0c801c5731ce396eb62f2f874738e070
Gabe <gabeshin.ts@gmail.com>
2023-12-03 11:06:58 +0900

    feat: stylcing ThemeGridCell

--- src/components/theme-cell/ThemeGridCell.tsx
+++ src/components/theme-cell/ThemeGridCell.tsx
@@ -17,15 +17,21 @@ export default function ThemeGridCell() {
   if (!mounted) return null;
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div
-        className="theme-button bg-gradient-to-r from-yellow-200 to-yellow-500"
-        onClick={() => handleClick("light")}
-      />
-      <button
-        className="theme-button bg-gradient-to-r from-gray-700 to-gray-900"
-        onClick={() => handleClick("dark")}
-      />
+    <div className="flex h-full w-full items-center justify-center ">
+      <div className="grid h-[180px] w-[180px] grid-cols-2 items-center justify-items-center gap-4">
+        <div
+          className="theme-button bg-gradient-to-r from-yellow-200 to-yellow-500"
+          onClick={() => handleClick("light")}
+        />
+        <div
+          className="theme-button bg-gradient-to-r from-gray-700 to-gray-900"
+          onClick={() => handleClick("dark")}
+        />
+        <div
+          className="theme-button bg-gradient-to-r from-green-700 to-red-700"
+          onClick={() => handleClick("retro")}
+        />
+      </div>
     </div>
   );
 }
