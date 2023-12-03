cff88c33b4ca917fdcf8ff36a1324612d846f725
Gabe <gabeshin.ts@gmail.com>
2023-12-03 13:14:47 +0900

    feat: button click theme cell

--- src/components/theme-cell/ThemeGridCell.tsx
+++ src/components/theme-cell/ThemeGridCell.tsx
@@ -1,40 +1,26 @@
 "use client";
-import { useState } from "react";
 import "./theme-grid-cell.css";
 import { useTheme } from "next-themes";
-import { useEffect } from "react";
 
 export default function ThemeGridCell() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
   const handleClick = (inputTheme: string) => {
     setTheme(inputTheme);
   };
 
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) return null;
-
   return (
     <div className="flex h-full w-full items-center justify-center ">
       <div className="grid h-[150px] w-[150px] grid-cols-2 items-center justify-items-center gap-4">
-        <div
-          className="clickable-button theme-button bg-gradient-to-r from-yellow-200 to-yellow-500"
-          onClick={() => handleClick("light")}
-        />
-        <div
-          className="clickable-button theme-button bg-gradient-to-r from-gray-700 to-gray-900"
-          onClick={() => handleClick("dark")}
-        />
-        <div
-          className="clickable-button theme-button bg-gradient-to-r from-green-700 to-red-700"
-          onClick={() => handleClick("retro")}
-        />
-        <div
-          className="clickable-button theme-button bg-gradient-to-r from-green-700 to-red-700"
-          onClick={() => handleClick("neon")}
-        />
+        {["light", "dark", "retro", "neon"].map((themeName) => (
+          <button
+            key={themeName}
+            className={`clickable-button theme-button ${themeName}-color-wheel ${
+              theme === themeName ? "active-theme" : ""
+            }`}
+            onClick={() => handleClick(themeName)}
+          />
+        ))}
       </div>
     </div>
   );
