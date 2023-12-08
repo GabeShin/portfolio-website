c6ab7d6ea65e0ede6cfdad9b5dcf571cba33bf49
Gabe <gabeshin.ts@gmail.com>
2023-12-08 11:55:36 +0900

    fix: style change

--- src/app/profile-cell/ProfileGridCell.tsx
+++ src/app/profile-cell/ProfileGridCell.tsx
@@ -5,9 +5,12 @@ export default function ProfileGridCell() {
         <p className="inline">Hello, I am </p>
         <h1 className="inline"> Gabe Shin</h1>
         <p className="inline">
-          , a web developer from South Korea currently based in Berlin. I am
-          interested in AI and look for ways to implement my knowledge of web
-          development with AI applications.
+          , a web developer from South Korea currently based in Berlin.{" "}
+        </p>
+        <p />
+        <p className="inline">
+          I am interested in applying AI in products and look for ways to
+          implement my knowledge of web development.
         </p>
       </div>
     </div>
